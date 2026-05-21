852be6b52d6f50cb535b8cd77ccd07c7335e8440
testano <you@example.com>
2026-05-21 20:05:08 -0300

    erros corrigidos?

--- cat.c
+++ cat.c
@@ -3,20 +3,21 @@
 #include <string.h>
 
 void processarDados() {
-    char buffer[10]; 
+    char buffer[100]; 
     strcpy(buffer, "Este texto é muito grande para o buffer");
 
     int *ptr = (int *)malloc(sizeof(int) * 5);
-    free(ptr);
     *ptr = 10;
+    free(ptr);
 
     int array[5];
-    array[5] = 100; 
+    array[4] = 100; 
 
-    int x = 10, y = 0;
+    int x = 10, y = 1;
     int z = x / y; 
 
-    char *memoria = (char *)malloc(100); 
+    char *memoria = (char *)malloc(100);
+    free(memoria); 
 }
 
 int main() {
